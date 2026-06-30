e0bcf1ebcddcae2968ee28aad32b559a8947e027
sa <g@timelabs.ru>
2026-06-30 02:07:18 -0700

    Initial Commit

---
+++ 1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  1
+//
+//  Created by mio on 6/30/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 1/_App.swift
@@ -0,0 +1,17 @@
+//
+//  _App.swift
+//  1
+//
+//  Created by mio on 6/30/26.
+//
+
+import SwiftUI
+
+@main
+struct _App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
